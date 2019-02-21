2e6ce4fc2338df92f4e24e1315daa9682269fd22
teckyuan <teckyuan9898@hotmail.com>
2019-02-21 12:38:18 +0800

    DATATABLE CONFIGURE

---
+++ WebApplicationmvc/Controllers/SlaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationmvc.Controllers
+{
+    public class SlaController : Controller
+    {
+        // GET: Sla
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
