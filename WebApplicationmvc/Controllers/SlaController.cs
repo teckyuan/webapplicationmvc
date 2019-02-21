a2c73a0930013a26a2088d1462f11356754cf742
teckyuan <teckyuan9898@hotmail.com>
2019-02-21 14:42:16 +0800

    post sla reach controller

--- WebApplicationmvc/Controllers/SlaController.cs
+++ WebApplicationmvc/Controllers/SlaController.cs
@@ -1,17 +1,51 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationmvc.Entity;
 
 namespace WebApplicationmvc.Controllers
 {
     public class SlaController : Controller
     {
+        private Logger logger;
+
+        public SlaController()
+        {
+            logger = LogManager.GetCurrentClassLogger();
+        }
         // GET: Sla
         public ActionResult Index()
         {
             return View();
         }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> sla(SLABE model)
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync("www.yahoo.com");
+                    return Json(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Info(ex);
+            }
+
+            return Json("Error Occured!");
+        }
+        // If we got this far, something failed, redisplay form
     }
-}
+
+    }
---
+++ WebApplicationmvc/Entity/SLABE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationmvc.Entity
+{
+    public class SLABE
+    {
+        public int id { get; set; }
+        public string desc { get; set; }
+        public int sladays { get; set; }
+    }
+}
